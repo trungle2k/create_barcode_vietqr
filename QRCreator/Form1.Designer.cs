bdb512cd86cb547a49daa0f552d0401befcc5b76
Trung <thanhtrung27022k@gmail.com>
2025-01-18 21:12:12 +0700

    Commit: base source

---
+++ QRCreator/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace QRCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtSTK = new TextBox();
+            txtTenTaiKhoan = new TextBox();
+            txtSoTien = new NumericUpDown();
+            label4 = new Label();
+            cb_template = new ComboBox();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            textBox1 = new TextBox();
+            label5 = new Label();
+            checkBox1 = new CheckBox();
+            ((System.ComponentModel.ISupportInitialize)txtSoTien).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Số tài khoản:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(27, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(80, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Tên tài khoản:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(27, 140);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Số tiền:";
+            // 
+            // txtSTK
+            // 
+            txtSTK.Location = new Point(27, 35);
+            txtSTK.Name = "txtSTK";
+            txtSTK.Size = new Size(185, 23);
+            txtSTK.TabIndex = 3;
+            txtSTK.KeyPress += txtSTK_KeyPress;
+            // 
+            // txtTenTaiKhoan
+            // 
+            txtTenTaiKhoan.Location = new Point(27, 95);
+            txtTenTaiKhoan.Name = "txtTenTaiKhoan";
+            txtTenTaiKhoan.Size = new Size(185, 23);
+            txtTenTaiKhoan.TabIndex = 4;
+            // 
+            // txtSoTien
+            // 
+            txtSoTien.Location = new Point(27, 162);
+            txtSoTien.Maximum = new decimal(new int[] { 9999999, 0, 0, 0 });
+            txtSoTien.Name = "txtSoTien";
+            txtSoTien.Size = new Size(185, 23);
+            txtSoTien.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(27, 320);
+            label4.Name = "label4";
+            label4.Size = new Size(77, 15);
+            label4.TabIndex = 6;
+            label4.Text = "QR Template:";
+            // 
+            // cb_template
+            // 
+            cb_template.DropDownStyle = ComboBoxStyle.DropDownList;
+            cb_template.FormattingEnabled = true;
+            cb_template.Items.AddRange(new object[] { "compact", "compact2", "qr_only", "print" });
+            cb_template.Location = new Point(27, 342);
+            cb_template.Name = "cb_template";
+            cb_template.Size = new Size(185, 23);
+            cb_template.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(264, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(401, 387);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(466, 430);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 9;
+            button1.Text = "Save image";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += buttonSaveImage_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(547, 430);
+            button2.Name = "button2";
+            button2.Size = new Size(118, 23);
+            button2.TabIndex = 10;
+            button2.Text = "Copy to clipboard";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += CopyImage_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(264, 412);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 48);
+            button3.TabIndex = 11;
+            button3.Text = "Create QR";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += buttonCreateQR_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(27, 405);
+            button4.Name = "button4";
+            button4.Size = new Size(80, 23);
+            button4.TabIndex = 12;
+            button4.Text = "Load file";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(27, 227);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(185, 81);
+            textBox1.TabIndex = 14;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(27, 204);
+            label5.Name = "label5";
+            label5.Size = new Size(125, 15);
+            label5.TabIndex = 13;
+            label5.Text = "Nội dung chuyển tiền:";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(27, 380);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(127, 19);
+            checkBox1.TabIndex = 15;
+            checkBox1.Text = "Create QR from file";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(693, 472);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(cb_template);
+            Controls.Add(label4);
+            Controls.Add(txtSoTien);
+            Controls.Add(txtTenTaiKhoan);
+            Controls.Add(txtSTK);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "BIDV QR Creator";
+            ((System.ComponentModel.ISupportInitialize)txtSoTien).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtSTK;
+        private TextBox txtTenTaiKhoan;
+        private NumericUpDown txtSoTien;
+        private Label label4;
+        private ComboBox cb_template;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox1;
+        private Label label5;
+        private CheckBox checkBox1;
+    }
+}
---
+++ QRCreator/Form1.cs
@@ -0,0 +1,269 @@
+using Newtonsoft.Json;
+using RestSharp;
+using System.Drawing.Imaging;
+
+namespace QRCreator
+{
+    public partial class Form1 : Form
+    {
+        private NotifyIcon notifyIcon;
+        private List<ApiRequest> m_lstApiRequests;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Create ContextMenuStrip
+            ContextMenuStrip contextMenu = new ContextMenuStrip();
+
+            // Add "Copy Image" menu item
+            ToolStripMenuItem copyImageItem = new ToolStripMenuItem("Copy Image");
+            copyImageItem.Click += CopyImageItem_Click;
+            contextMenu.Items.Add(copyImageItem);
+
+            // Assign ContextMenuStrip to PictureBox
+            pictureBox1.ContextMenuStrip = contextMenu;
+
+            cb_template.SelectedIndex = 1;
+
+            // Initialize NotifyIcon
+            notifyIcon = new NotifyIcon
+            {
+                Icon = SystemIcons.Information, // Use a built-in icon or your own
+                Visible = true, // Make it visible in the system tray
+                BalloonTipTitle = "Notification Title", // Title for the balloon tip
+                BalloonTipText = "This is your notification message.", // Message text
+                BalloonTipIcon = ToolTipIcon.Info // Icon type
+            };
+
+            m_lstApiRequests = new List<ApiRequest>();
+        }
+
+        private void CopyImageItem_Click(object? sender, EventArgs e)
+        {
+            CopyImageToClipboard();
+        }
+
+        private void buttonCreateQR_Click(object sender, EventArgs e)
+        {
+            var apiRequest = new ApiRequest();
+            apiRequest.acqId = 970418;
+            apiRequest.accountNo = long.Parse(txtSTK.Text);
+            apiRequest.accountName = txtTenTaiKhoan.Text;
+            apiRequest.amount = Convert.ToInt32(txtSoTien.Text);
+            apiRequest.format = "text";
+            apiRequest.template = cb_template.Text;
+            var jsonRequest = JsonConvert.SerializeObject(apiRequest);
+            // use restsharp for request api.
+            var client = new RestClient("https://api.vietqr.io/v2/generate");
+            var request = new RestRequest();
+
+            request.Method = Method.Post;
+            request.AddHeader("Accept", "application/json");
+
+            request.AddParameter("application/json", jsonRequest, ParameterType.RequestBody);
+
+            RestResponse response = client.Execute(request);
+            string? content = response.Content;
+            if (string.IsNullOrEmpty(content) == false)
+            {
+                ApiResponse? dataResult = JsonConvert.DeserializeObject<ApiResponse>(content);
+                if (dataResult != null && dataResult.data != null && dataResult.data.qrDataURL != null)
+                {
+                    Image image = Base64ToImage(dataResult.data.qrDataURL.Replace("data:image/png;base64,", ""));
+                    pictureBox1.Image = image;
+                }
+            }
+        }
+
+        public Image Base64ToImage(string base64String)
+        {
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length);
+            ms.Write(imageBytes, 0, imageBytes.Length);
+            System.Drawing.Image image = System.Drawing.Image.FromStream(ms, true);
+            return image;
+        }
+
+        private void CopyImage_Click(object sender, EventArgs e)
+        {
+            CopyImageToClipboard();
+        }
+
+        private void CopyImageToClipboard()
+        {
+            // Ensure the PictureBox contains an image
+            if (pictureBox1.Image != null)
+            {
+                try
+                {
+                    Clipboard.SetImage(pictureBox1.Image);
+                    ShowNotification("Success", "Image copied to clipboard successfully!");
+                }
+                catch (Exception ex)
+                {
+                    ShowNotification("Error", $"Error copying image: {ex.Message}");
+                }
+            }
+            else
+            {
+                ShowNotification("Warning", "No image in PictureBox to copy.");
+            }
+        }
+
+        private void buttonSaveImage_Click(object sender, EventArgs e)
+        {
+            // Ensure the PictureBox contains an image
+            if (pictureBox1.Image != null)
+            {
+                try
+                {
+                    // Show a SaveFileDialog to choose file path and name
+                    using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                    {
+                        saveFileDialog.Filter = "JPEG Image|*.jpg|PNG Image|*.png|Bitmap Image|*.bmp";
+                        saveFileDialog.Title = "Save an Image File";
+                        saveFileDialog.FileName = "Image";
+
+                        if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            // Save the image in the selected format
+                            string filePath = saveFileDialog.FileName;
+
+                            // Determine the format based on file extension
+                            ImageFormat format = ImageFormat.Png; // Default
+                            if (filePath.EndsWith(".jpg"))
+                                format = ImageFormat.Jpeg;
+                            else if (filePath.EndsWith(".bmp"))
+                                format = ImageFormat.Bmp;
+
+                            pictureBox1.Image.Save(filePath, format);
+                            MessageBox.Show("Image saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error saving image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No image in PictureBox to save.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void txtSTK_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Allow only numbers, control keys (e.g., backspace), and decimal points
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; // Reject the input
+            }
+        }
+
+        public void ShowNotification(string title, string message)
+        {
+            if (notifyIcon != null)
+            {
+                notifyIcon.BalloonTipTitle = title;
+                notifyIcon.BalloonTipText = message;
+                notifyIcon.ShowBalloonTip(3000); // Show for 3 seconds
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // Cleanup NotifyIcon when the form closes
+            notifyIcon.Dispose();
+            base.OnFormClosing(e);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Open File Dialog to choose Excel file
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Excel Files|*.csv";
+                openFileDialog.Title = "Select an CSV File";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+
+                    using (StreamReader reader = new StreamReader(filePath))
+                    {
+                        string? line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            var values = line.Split(';'); // Split using semicolon
+                            var record = new ApiRequest
+                            {
+                                accountNo = long.Parse(txtSTK.Text),
+                                accountName = txtTenTaiKhoan.Text,
+                                acqId = 970418,
+                                amount = int.Parse(values[3]),
+                                addInfo = values[1] + " " + values[2],
+                                format = "text",
+                                template = "compact2"
+                            };
+                            m_lstApiRequests.Add(record);
+                        }
+                    }
+
+                    if (m_lstApiRequests.Count > 0)
+                    {
+                        int i = 0;
+                        foreach (var record in m_lstApiRequests)
+                        {
+                            var jsonRequest = JsonConvert.SerializeObject(record);
+                            // use restsharp for request api.
+                            var client = new RestClient("https://api.vietqr.io/v2/generate");
+                            var request = new RestRequest();
+
+                            request.Method = Method.Post;
+                            request.AddHeader("Accept", "application/json");
+
+                            request.AddParameter("application/json", jsonRequest, ParameterType.RequestBody);
+
+                            RestResponse response = client.Execute(request);
+                            string? content = response.Content;
+                            if (string.IsNullOrEmpty(content) == false)
+                            {
+                                ApiResponse? dataResult = JsonConvert.DeserializeObject<ApiResponse>(content);
+                                if (dataResult != null && dataResult.data != null && dataResult.data.qrDataURL != null)
+                                {
+                                    Image image = Base64ToImage(dataResult.data.qrDataURL.Replace("data:image/png;base64,", ""));
+
+                                    // Save the image
+                                    string saveDirectory = @"C:\Images";
+                                    if (!Directory.Exists(saveDirectory))
+                                    {
+                                        Directory.CreateDirectory(saveDirectory);
+                                    }
+
+                                    string fileName = $"{i++}_QR.png";
+                                    string savePath = Path.Combine(saveDirectory, fileName);
+
+                                    image.Save(savePath, System.Drawing.Imaging.ImageFormat.Png);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+
+            }
+            else 
+            { 
+
+            }
+        }
+    }
+}
