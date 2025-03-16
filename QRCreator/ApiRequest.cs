368b295fc86c57fb40c52f2c43b543439b4e3f6a
Lê Trung <57955843+trungle2k@users.noreply.github.com>
2025-03-16 17:48:45 +0700

    commit 1

--- QRCreator/ApiRequest.cs
+++ QRCreator/ApiRequest.cs
@@ -5,6 +5,7 @@
         public long accountNo { get; set; }
         public string? accountName { get; set; }
         public int acqId { get; set; }
+        // Test: Coding 1
         public int amount { get; set; }
         public string? addInfo { get; set; }
         public string? format { get; set; }
